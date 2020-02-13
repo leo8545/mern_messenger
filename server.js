e1dc8cec6b7c110410637d3fc0fa39af2f6ae7fa
sharjeel <leo8545@gmail.com>
2020-02-14 01:23:43 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+	.connect("mongodb://localhost/approckmsg", {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	})
+	.then(() => console.log("Connected to mongodb"))
+	.catch(err => console.log("couldnot connect to mongodb", err));
+
+/**
+ * Routes
+ */
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/messages", require("./routes/api/messages"));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
