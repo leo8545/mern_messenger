81e634bf4748413d2c6c2ed0eb2a97464394148f
sharjeel <leo8545@gmail.com>
2020-02-14 15:29:57 +0500

    Fixing db

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/messages", require("./routes/api/messages"));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-	// Set static folder
-	app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+// 	// Set static folder
+// 	app.use(express.static("client/build"));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// 	});
+// }
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
