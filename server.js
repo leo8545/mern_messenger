c3d05ff1b92e7e8a6ad250b09589a6509390a5e2
sharjeel <leo8545@gmail.com>
2020-02-14 16:02:12 +0500

    Fixing db

--- server.js
+++ server.js
@@ -6,18 +6,14 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+const db = process.env.MONGODB_URL;
 mongoose
-	.connect(
-		"mongodb//sharjeel:sYmbolic*9@approck-mai7f.mongodb.net/mern_messenger_db" ||
-			"mongodb://localhost/approckmsg",
-		{
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			useFindAndModify: false,
-			useCreateIndex: true
-		}
-	)
+	.connect(db, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	})
 	.then(() => console.log("Connected to mongodb"))
 	.catch(err => console.log("couldnot connect to mongodb", err));
 
@@ -29,13 +25,13 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/messages", require("./routes/api/messages"));
 
 // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-// 	// Set static folder
-// 	app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
 
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
